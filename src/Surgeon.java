4f0501fc1221dff70acb999b707f99fa59eb2420
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-07-19 12:48:00 -0400

    first commit

---
+++ src/Surgeon.java
@@ -0,0 +1,7 @@
+public class Surgeon extends Doctor{
+    void Surgeon_Details() {
+        System.out.println("Surgeon   Detail...");
+        System.out.println(Department = "Cardio");
+
+    }
+}
